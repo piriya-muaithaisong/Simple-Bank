34e46731c97abb2bbca49fe0c9cc6860fb83b4b7
Piriya <piriya.muaithaisong@gmail.com>
2023-01-13 20:38:28 +0700

    Addinhcustom validator

---
+++ api/validator.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/piriya-muaithaisong/Simple-Bank/utils"
+)
+
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		return utils.IsSupportedCurrency(currency)
+	}
+	return false
+
+}
