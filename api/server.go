5fcc319c9e021b7514a5bd4fd106f4edc2add1e9
Piriya <piriya.muaithaisong@gmail.com>
2023-01-15 21:44:08 +0700

    Implemented login function

--- api/server.go
+++ api/server.go
@@ -1,35 +1,42 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/piriya-muaithaisong/Simple-Bank/db/sqlc"
+	"github.com/piriya-muaithaisong/Simple-Bank/token"
+	"github.com/piriya-muaithaisong/Simple-Bank/utils"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     utils.Config
+	store      db.Store
+	router     *gin.Engine
+	tokenMaker token.Maker
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config utils.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		store:      store,
+		tokenMaker: tokenMaker,
+		config:     config,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	//add router
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
+	server.setupRouter()
 
-	router.POST("/transfers", server.createTransfer)
-	router.POST("/users", server.createUser)
-
-	server.router = router
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(address string) error {
@@ -38,4 +45,19 @@ func (server *Server) Start(address string) error {
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	router.POST("/transfers", server.createTransfer)
+
+	router.POST("/users", server.createUser)
+
+	router.POST("users/login", server.loginUser)
+
+	server.router = router
 }
