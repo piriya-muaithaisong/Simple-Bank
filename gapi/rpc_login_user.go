26fbbaa718866038bd910cf14f5797aae429940d
Piriya <piriya.muaithaisong@gmail.com>
2023-01-23 20:36:11 +0700

    create error handling and validation on LoginUser API

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,12 +7,19 @@ import (
 	db "github.com/piriya-muaithaisong/Simple-Bank/db/sqlc"
 	"github.com/piriya-muaithaisong/Simple-Bank/pb"
 	"github.com/piriya-muaithaisong/Simple-Bank/utils"
+	"github.com/piriya-muaithaisong/Simple-Bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
@@ -70,4 +77,17 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	}
 
 	return res, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
+
 }
